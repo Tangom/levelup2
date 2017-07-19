5c8acb4c97633f4556480cd72b20adf25d189e4e
Protcko, Dmitrii <Dmitrii.Protcko@krongshtadt.ru>
2017-07-19 13:52:34 +0300

    Lesson 3

---
+++ src/main/java/org/levelup/list/OneWayList.java
@@ -0,0 +1,170 @@
+package org.levelup.list;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Created by Student-18 on 18.07.2017.
+ */
+public class OneWayList<T> implements List<T> {
+
+    private Element head;       // first element
+    private Element tail;       // last element
+    private int size;           // count of elements
+
+    private class Element{
+        T value;
+        Element next;
+        Element(T value) {
+            this.value = value;
+        }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T t) {
+        Element element = new Element(t);
+        if (tail == null) {
+            head = element;
+            tail = element;
+        } else {
+            tail.next = element;
+            tail = element;
+        }
+        size++;
+        return true;
+    }
+
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    public void clear() {
+        head = tail = null;
+    }
+
+    public T get(int index) {
+        checkIndex(index);
+
+        Element current = head;
+        int currIndex = 0;
+        while (currIndex != index) {
+            currIndex++;
+            current = current.next;
+        }
+        return current.value;
+    }
+
+    public T set(int index, T element) {
+        return null;
+    }
+
+    public void add(int index, T element) {    }
+
+    @Override
+    public T remove(int index) {
+        checkIndex(index);
+
+        if (size == 1) {
+            T value = head.value;
+            head = null;
+            tail = null;
+            size--;
+            return value;
+        }
+
+        if (index == 0) {
+            T value = head.value;
+            head = head.next;
+            size--;
+            return value;
+        }
+
+        Element curr = head;
+        Element prev = null;
+        int currIndex = 0;
+        while (currIndex != index) {
+            prev = curr;
+            curr = curr.next;
+            currIndex++;
+        }
+
+        prev.next = curr.next;
+        size--;
+        return curr.value;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index cannot be less than zero or " +
+                    "more than count of elements. Index = " + index);
+        }
+    }
+
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
---
+++ src/test/java/org/levelup/list/OneWayListTest.java
@@ -0,0 +1,106 @@
+package org.levelup.list;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Student-18 on 18.07.2017.
+ */
+public class OneWayListTest {
+
+    private List<String> list;
+
+    @Before
+    public void setup() {
+        list = new OneWayList<>();
+    }
+
+    @Test
+    public void testSize() {
+        assertEquals(0, list.size());
+        list.add("Some string");
+        assertEquals(1, list.size());
+    }
+
+    @Test
+    public void testIsEmpty() {
+        assertTrue(list.isEmpty());
+        list.add("Some string");
+        assertFalse(list.isEmpty());
+    }
+
+    @Test
+    public void testAdd() {
+        // when
+        boolean result = list.add("String1");
+
+        // then
+        assertTrue(result);
+        assertEquals(1, list.size());
+        assertFalse(list.isEmpty());
+    }
+
+    //
+    // Tests for method "get(int index)"
+    //
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetWithNegativeIndex() {
+        list.get(-3);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetWithIndexMoreThanSize() {
+        list.add("Some string1");
+        list.add("Some string2");
+        list.add("Some string3");
+        list.add("Some string4");
+
+        list.get(4);
+    }
+
+    @Test
+    public void testGet() {
+        list.add("Some string1");
+        list.add("Some string2");
+        list.add("Some string3");
+
+        assertEquals("Some string1", list.get(0));
+        assertEquals("Some string2", list.get(1));
+        assertEquals("Some string3", list.get(2));
+    }
+
+    //
+    // Tests for method "remove(int index)"
+    //
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testRemoveWithNegativeIndex() {
+        list.remove(-1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testRemoveWithIndexMoreThanSize() {
+        list.add("some string1");
+        list.remove(1);
+    }
+
+    @Test
+    public void testRemove() {
+        list.add("Some string1");
+        assertEquals("Some string1", list.remove(0));
+        assertEquals(0, list.size());
+
+        list.add("Some string2");
+        list.add("Some string3");
+
+        assertEquals("Some string2", list.remove(0));
+        assertEquals(1, list.size());
+    }
+
+
+}
